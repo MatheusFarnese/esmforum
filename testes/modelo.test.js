229c8cbfc1587f72e44660e3dfeb62d192e3ff13
MatheusFarnese <95251433+MatheusFarnese@users.noreply.github.com>
2023-11-07 01:07:14 -0300

    Tarefa 2

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -28,7 +28,6 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
-
   expect(resposta0[0].texto).toBe('2');
   expect(resposta2[0].texto).toBe('6');
 });
